70bf35ec5389b4fcffa4fe3e8b2e5dac8e1cadb4
Sebastien Dupont <sebastien.dupont@cetic.be>
2020-08-30 00:43:54 +0200

    Laravel local backup

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
 
+        Spatie\Backup\BackupServiceProvider::class,
     ],
 
     /*
