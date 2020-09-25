e53a012700b5b314aad0285645654821b2eaa653
Thierry Guedj <thguedj@gmail.com>
2020-09-25 17:57:18 +0200

    Updated RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return $this->users->create([
-            'username' => $data['username'],
+            // 'username' => $data['username'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
