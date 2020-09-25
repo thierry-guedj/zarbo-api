28e8aa53c7d0e3d1576d7da854486c5d4b8a13ce
Thierry Guedj <thguedj@gmail.com>
2020-09-26 01:58:43 +0200

    Updated header email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,9 +4,10 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="{{ $message->embed(public_path().'/images/logo192x192.png') }}" style="padding:0px; margin:0px" />
+<img src="<?php echo $message->embed($pathToFile); ?>">
 {{ $slot }}
 @endif
+<img src="{{ $message->embed(public_path().'/images/logo192x192.png') }}" style="padding:0px; margin:0px" />
 </a>
 </td>
 </tr>
