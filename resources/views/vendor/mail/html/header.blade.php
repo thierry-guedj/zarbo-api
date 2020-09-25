416988a8e8bb144362710124e12ad53380ca0cfb
Thierry Guedj <thguedj@gmail.com>
2020-09-26 01:14:57 +0200

    Updated header.blade

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,9 +4,9 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
+<img src="{{ $message->embed(public_path().'/images/logo192x192.png') }}" style="padding:0px; margin:0px" />
 {{ $slot }}
 @endif
-popol
 </a>
 </td>
 </tr>
