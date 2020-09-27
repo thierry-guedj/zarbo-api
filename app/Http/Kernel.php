58a87a4161c99633d28c905f282f5d4f4b05dcf6
Thierry Guedj <thguedj@gmail.com>
2020-09-27 02:45:44 +0200

    Updated translations

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\ProfileJsonResponse::class,
-        \App\Http\Middleware\Localization::class, // Localization Middleware
+        
     ];
 
     /**
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Localization::class, // Localization Middleware
         ],
 
         'api' => [
