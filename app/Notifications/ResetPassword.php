1e8ddf8518d1fbea148c85eabef717d99e1f390e
Thierry Guedj <thguedj@gmail.com>
2020-10-23 19:10:02 +0200

    Updated ResetPassword

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Auth\Notifications\ResetPassword as Notification;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
 
 
 class ResetPassword extends Notification
@@ -19,9 +20,9 @@ class ResetPassword extends Notification
                     '?email='.urlencode($notifiable->email);
 
         return (new MailMessage)
-                    ->line('You are receiving this email because we received a password reset request for your account.')
-                    ->action('Reset Password', $url)
-                    ->line('If you did not request a password reset, no further action is required.');
+                    ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+                    ->action(Lang::get('Reset Password'), $url)
+                    ->line(Lang::get('If you did not request a password reset, no further action is required.'));
     }
 
 }
